28ddefc5965a5a9addf651b7bb016381c21f6b2d
alinagunich2 <gunich.alina@gmail.com>
2024-05-25 17:13:03 +0300

    finished

---
+++ src/Collection.jsx
@@ -0,0 +1,17 @@
+const Collection = ({ name, images }) => {
+  return (
+    <div className="collection">
+      {images.length > 0 && (
+        <img className="collection__big" src={images[0]} alt="Item" />
+      )}
+      <div className="collection__bottom">
+        <img className="collection__mini" src={images[1]} alt="Item" />
+        <img className="collection__mini" src={images[2]} alt="Item" />
+        <img className="collection__mini" src={images[3]} alt="Item" />
+      </div>
+      <h4>{name}</h4>
+    </div>
+  );
+};
+
+export default Collection;
